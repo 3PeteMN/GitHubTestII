b9116df9c8a9b97d50d5a219f9f5bc47b99974c5
3PeteMN <3pmeyer@comcast.net>
2019-11-03 12:50:33 -0600

    Added a comment

--- GitHubLearning/GitHubLearning/Program.cs
+++ GitHubLearning/GitHubLearning/Program.cs
@@ -14,6 +14,7 @@ namespace GitHubLearning
             //code was added in GitHub
             //GitHub change Three
             //GitHub change Fourth
+            //VS code change
         }
     }
 }
