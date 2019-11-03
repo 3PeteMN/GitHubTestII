32f3ca3b9e7df81fb2b476038b8fece914ba6757
3PeteMN <3pmeyer@comcast.net>
2019-11-03 12:30:26 -0600

    First Commit

---
+++ GitHubLearning/GitHubLearning/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
