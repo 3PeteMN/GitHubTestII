3d500b77ccb8536d208a66673125cab2aaeb8f3a
3PeteMN <3pmeyer@comcast.net>
2019-11-03 13:58:16 -0600

    Modified Class1 in master branch

--- GitHubLearning/GitHubLearning/Class1.cs
+++ GitHubLearning/GitHubLearning/Class1.cs
@@ -8,5 +8,15 @@ namespace GitHubLearning
 {
     class Class1
     {
+
+        public int Add()
+        {
+
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1 + x2;
+
+            return sum;
+        }
     }
 }
